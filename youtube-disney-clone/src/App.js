96ab1b7811bae35d2089346270e63f529baa77e8
Bhavya Haria <bhavya.haria@sakec.ac.in>
2022-12-21 15:21:00 +0530

    added login component

--- youtube-disney-clone/src/App.js
+++ youtube-disney-clone/src/App.js
@@ -1,9 +1,18 @@
+// import { Router } from "react-router"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import Login from "./components/Login.js"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h2>Hello</h2>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Login />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
