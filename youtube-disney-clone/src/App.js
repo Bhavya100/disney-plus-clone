8547d01e5a6c383b7fd75535781f73eead8b53b5
Bhavya Haria <bhavya.haria@sakec.ac.in>
2022-12-21 15:41:51 +0530

    added google login popup to login button

--- youtube-disney-clone/src/App.js
+++ youtube-disney-clone/src/App.js
@@ -1,11 +1,15 @@
 // import { Router } from "react-router"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import Login from "./components/Login.js"
+import Header from "./components/Header.js"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Router>  
+        <Header/>
+      </Router>
       <Router>
         <Switch>
           <Route exact path="/">
